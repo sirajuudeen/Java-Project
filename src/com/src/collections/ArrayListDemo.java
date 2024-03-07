5484150a859e5ec9ee5c0a80cd31a4103f89024a
sirajuudeen <152366047+sirajuudeen@users.noreply.github.com>
2024-03-07 21:29:42 +0530

    ArrayList changes

--- src/com/src/collections/ArrayListDemo.java
+++ src/com/src/collections/ArrayListDemo.java
@@ -4,14 +4,21 @@ import java.util.*;
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
-		        // Creating an Array of string type
-		        ArrayList<String> al = new ArrayList<>();
-		 
-		        al.add("Jaas");
-		        al.add("Aass");
-		        al.add(1, "Aahil");
-		 
-		        // Printing all the elements in an ArrayList
-		        System.out.println(al);
-		    }
+		
+		List<Integer> list = new ArrayList<Integer>();
+
+		list.add(2);
+		list.add(1);
+		list.add(1, 3);
+		list.add(1);
+		list.add(4);
+		Collections.reverse(list); // Reverse the array
+
+		Set<Integer> set=new LinkedHashSet<Integer>(list); // Remove the Duplicate value
+		list.clear();
+		list.addAll(set);
+
+		// Printing all the elements in an ArrayList
+		System.out.println(list);
+	}
 }
