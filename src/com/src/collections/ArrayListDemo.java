e3713b22be1ea25d0e6ab2692f1e9fe7b1d47df8
sirajuudeen <152366047+sirajuudeen@users.noreply.github.com>
2024-03-07 21:11:38 +0530

    commit

---
+++ src/com/src/collections/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.src.collections;
+import java.util.*;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		        // Creating an Array of string type
+		        ArrayList<String> al = new ArrayList<>();
+		 
+		        al.add("Jaas");
+		        al.add("Aass");
+		        al.add(1, "Aahil");
+		 
+		        // Printing all the elements in an ArrayList
+		        System.out.println(al);
+		    }
+}
